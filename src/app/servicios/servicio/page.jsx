041273b548c037b61deec63c61b7f7544456aa82
Rosario Martinez <111587388+romartinez927@users.noreply.github.com>
2023-07-28 16:21:06 -0300

    fb

--- src/app/servicios/servicio/page.jsx
+++ src/app/servicios/servicio/page.jsx
@@ -1,4 +1,4 @@
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 import { faGlobe, faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
