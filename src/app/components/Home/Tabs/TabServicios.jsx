6f556219332f8365a9ddab9233966079bd28d6d7
Rosario Martinez <111587388+romartinez927@users.noreply.github.com>
2023-08-04 17:49:56 -0300

    detalles home

--- src/app/components/Home/Tabs/TabServicios.jsx
+++ src/app/components/Home/Tabs/TabServicios.jsx
@@ -4,10 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function TabServicios() {
     return (
         <form action="" className="formHeader rounded py-3 mb-3 px-4 d-flex mx-auto justify-content-between gap-3">
-            <div className='d-flex align-items-center' style={{width: "80%"}}>
+            <div className='tabServicios d-flex align-items-center'>
                 <input className="form-control w-100 customSelect py-3 ps-4" type="search" placeholder="Buscar Servicios..." aria-label="Search" />
             </div>
-            <div>
+            <div className='mx-auto'>
                 <button className="btn-red btn fw-semibold py-3 px-4">
                     <FontAwesomeIcon icon={faMagnifyingGlass} className='fs-6 me-2' />
                     Buscar
